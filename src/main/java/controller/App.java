b81f2abc0aec2fa14d9ba99b07e5456716a77648
NathanSaludes <201502034@iacademy.edu.ph>
2019-08-21 06:34:32 +0800

    fixed App servlet issue

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -20,27 +20,37 @@ import utility.Logger;
 public class App extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	public App() {
+		
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Logger.log(" --- FIRESHIP SHOPPING STARTED --- ");
-		Logger.log("INITIALIZING APP CONFIGURATION");
 		
-		Config c = new Config();
-		
-		Logger.log("INITIALIZING OBJECTS");
-		
-		// INITIALIZE BOJECTS
-		ArrayList<Product> product_list = c.getProducts();
-		History order_history = new History();
-		Order order = new Order();
-		order.setVatRate(Float.parseFloat(getServletContext().getInitParameter("VAT_RATE")));
-		
-		// BIND OBJECTS TO SERVLET CONTEXT
-		ServletContext sc = getServletContext();
-		sc.setAttribute("PRODUCTS_LIST", product_list);
-		sc.setAttribute("ORDER_HISTORY", order_history);
-		sc.setAttribute("ORDER", order);
-		
-		response.sendRedirect("home.jsp");
+		if(getServletContext().getAttribute("ORDER") == null) {
+			Logger.log(" --- FIRESHIP SHOPPING STARTED --- ");
+			Logger.log("INITIALIZING APP CONFIGURATION");
+			
+			Config c = new Config();
+			
+			Logger.log("INITIALIZING OBJECTS");
+			
+			// INITIALIZE BOJECTS
+			ArrayList<Product> product_list = c.getProducts();
+			History order_history = new History();
+			Order order = new Order();
+			order.setVatRate(Float.parseFloat(getServletContext().getInitParameter("VAT_RATE")));
+			
+			// BIND OBJECTS TO SERVLET CONTEXT
+			ServletContext sc = getServletContext();
+			sc.setAttribute("PRODUCTS_LIST", product_list);
+			sc.setAttribute("ORDER_HISTORY", order_history);
+			sc.setAttribute("ORDER", order);
+			
+			response.sendRedirect("home.jsp");
+			
+		} else {			
+			response.sendRedirect("home.jsp");		
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
